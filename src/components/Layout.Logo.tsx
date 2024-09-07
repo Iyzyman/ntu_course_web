4b0605fe27d6e609a76fd93a6028841a7f7eae96
Iyzyman <101888183+Iyzyman@users.noreply.github.com>
2024-09-07 21:43:43 +0800

    feat: refactor home page

--- src/components/Layout.Logo.tsx
+++ src/components/Layout.Logo.tsx
@@ -1,34 +1,46 @@
-import { Badge } from '@/components/ui/Badge'
-import { env } from '@/env'
-import { cn } from '@/utils/dom'
-import { useNavigate } from 'react-router-dom'
+import { cn } from '@/utils/dom';
+import { useNavigate } from 'react-router-dom';
 
 export const Logo = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div
-      className={cn('flex flex-row place-items-center gap-1', 'cursor-pointer')}
-      onClick={() => {
-        navigate(`/`, {
-          unstable_viewTransition: true,
-        })
-      }}
-    >
-      <b
-        className={cn(
-          'max-w-prose text-center tracking-tight',
-          'h3 sm:h2 whitespace-nowrap',
-        )}
-      >
-        📚 shelvd
-      </b>
 
-      {env.VITE_BETA_FLAG && (
-        <Badge className="-translate-y-2/4 px-1 text-[0.5rem] uppercase">
-          beta
-        </Badge>
-      )}
-    </div>
-  )
-}
+      <div
+      className={cn('flex flex-row place-items-center gap-1', 'cursor-pointer')}
+        onClick={() => {
+          navigate(`/`, {
+            unstable_viewTransition: true,
+          });
+        }}
+    
+      >
+        <b
+          className={cn(
+            'max-w-prose text-center tracking-tight',
+            'h3 sm:h2 whitespace-nowrap',
+          )}
+        >
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
+            <div
+              className='logo'
+              style={{
+                fontFamily: 'Bebas Neue',
+                fontWeight: '400',
+                fontSize: '3rem',
+                paddingRight: '0.5rem',
+              }}
+            >
+              CF
+            </div>
+            <span style={{ fontSize: '1.5rem', fontWeight: '400' }}>coursefinder</span>
+          </div>
+
+        </b>
+      </div>
+  );
+};
--- src/components/Layout.Search.tsx
+++ src/components/Layout.Search.tsx
@@ -1241,8 +1241,8 @@ export const SearchCommandTrigger = () => {
       </aside>
 
       <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-        <span className="text-xs">⌘</span>
-        {AppCommandKey.toUpperCase()}
+        <span className="text-xs">Crtl + {AppCommandKey.toUpperCase()}</span>
+        
       </kbd>
     </div>
   )
